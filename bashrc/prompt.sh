0bdc1d3e664fff9f68be2bbbf0738aa86c7e5beb
Mack Ward <mackorone@gmail.com>
2022-08-09 01:39:46 -0400

    Print nonzero exit codes

--- bashrc/prompt.sh
+++ bashrc/prompt.sh
@@ -1,7 +1,26 @@
 #!/usr/bin/env bash
 
-PS1='\[\e[0;36m\]{\W}$\[\e[m\] '
+COLOR_LIGHT_RED='\[\e[1;31m\]'
+COLOR_TURQUOISE='\[\e[0;36m\]'
+COLOR_RESET='\[\e[m\]'
 
-PS1='$(show_virtual_env)'$PS1
-PS1='$(show_op_session_active)'$PS1
-PS1='$(show_secrets_are_present)'$PS1
+print_if_nonzero() {
+    local value="$1"
+    if [ "$value" != 0 ]; then
+        # shellcheck disable=SC2028
+        echo "${COLOR_LIGHT_RED}[$value]${COLOR_RESET} \n"
+    fi
+}
+
+prompt_command() {
+    local exit_code="$?"
+    prompt=''
+    prompt+="$(print_if_nonzero $exit_code)"
+    prompt+="$(show_secrets_are_present)"
+    prompt+="$(show_op_session_active)"
+    prompt+="$(show_virtual_env)"
+    prompt+="${COLOR_TURQUOISE}{\W}\$${COLOR_RESET} "
+    export PS1="$prompt"
+}
+
+export PROMPT_COMMAND=prompt_command
